ea32cf239cb19931a39806ce083bd9bebad14b96
Alfred Cheung <36896899+alfredphysarani@users.noreply.github.com>
2023-12-22 18:21:31 +0800

    fix issues and added site map

--- Shopping-Demo/script_main.js
+++ Shopping-Demo/script_main.js
@@ -8,71 +8,72 @@ let productArray = [
     {
         "id": "0",
         "name": "Cat Food",
-        "price": "40.00",
+        "previousPrice": "80.00",
+        "currentPrice": "40.00",
         "img": "cat-food.png",
-        "desc": "Best for 0-2 years old cat. Tuna flavour",
         "url": "prod/product_cat-food.html"
     },
     {
         "id": "1",
         "name": "Dog Food",
-        "price": "35.00",
+        "previousPrice": "70.00",
+        "currentPrice": "35.00",
         "img": "dog-food.webp",
-		"desc": "Nutritious and delicious dog food for every stage of life.",
 		"url": "./prod/product_dog-food.html"
     },
     {
         "id": "2",
         "name": "Candy",
-        "price": "20.00",
+        "previousPrice": "25.00",
+        "currentPrice": "20.00",
         "img": "candy.png",
-		"desc": "Sweet treats for every occasion.",
 		"url": "./prod/product_candy.html"
     },
     {
         "id": "3",
         "name": "Children Book",
-        "price": "10.00",
+        "previousPrice": "11.00",
+        "currentPrice": "10.00",
         "img": "children-book.png",
-		"desc": "A magical adventure for young readers.",
 		"url": "./prod/product_children-book.html"
     },
     {
         "id": "4",
         "name": "Potato Chips",
-        "price": "5.00",
+        "previousPrice": "10.00",
+        "currentPrice": "5.00",
         "img": "potato-chips.svg",
-		"desc": "Crunchy and flavorful potato chips for snacking.",
 		"url": "./prod/product_potato-chips.html"
     },
     {
         "id": "5",
         "name": "Football",
-        "price": "55.20",
+        "previousPrice": "58.10",
+        "currentPrice": "55.20",
         "img": "football.png",
-		"desc": "A durable and high-quality football for playing and training.",
 		"url": "./prod/product_football.html"
     },
     {
         "id": "6",
         "name": "Basketball",
-        "price": "45.20",
+        "previousPrice": "47.60",
+        "currentPrice": "45.20",
         "img": "basketball.png",
-		"desc": "A professional and reliable basketball for competitive and recreational games.",
 		"url": "./prod/product_basketball.html"
     },
     {
         "id": "7",
         "name": "Phone Case",
-        "price": "500.00",
+        "previousPrice": "550.00",
+        "currentPrice": "500.00",
         "img": "phone-case.webp",
-		"desc": "A stylish and protective phone case for your smartphone.",
 		"url": "./prod/product_phone-case.html"
     },
     {
         "id": "8",
         "name": "Apple",
-        "price": "10.00",
+        "previousPrice": "11.00",
+        "currentPrice": "10.00",
         "img": "apple.svg",
 		"desc": "A fresh and juicy apple for a healthy snack.",
 		"url": "./prod/product_apple.html"
@@ -80,7 +81,8 @@ let productArray = [
     {
         "id": "9",
         "name": "Math Book",
-        "price": "8.00",
+        "previousPrice": "8.90",
+        "currentPrice": "8.00",
         "img": "math-book.svg",
 		"desc": "A comprehensive and easy-to-follow math book for students and teachers.",
 		"url": "./prod/product_math-book.html"
@@ -88,13 +90,13 @@ let productArray = [
     {
         "id": "10",
         "name": "Egg",
-        "price": "20.00",
+        "previousPrice": "21.00",
+        "currentPrice": "20.00",
         "img": "egg.png",
 		"desc": "Fresh and nutritious eggs for cooking and baking.",
 		"url": "./prod/product_egg.html"
     }
 ]
-
 
 /* 
     Product List Related Function
@@ -117,9 +119,10 @@ function productList(event) {
         if (productArray[i].name.toLowerCase().includes(filterWord) || filterWord === "") {
             prodContainer.innerHTML += "<div class='product-container' id='" +
                 productArray[i].id + "'><a href='" + productArray[i].url + "'><img src='img/" +
-                productArray[i].img + "'><p>" +
+                productArray[i].img + "'><p class='prod-list-name'>" +
                 productArray[i].name + "</p>" +
-                "<p>Price: $ " + productArray[i].price + "</p></a></div>";
+                "<p>Price: <span class='previous-price'>$" + productArray[i].previousPrice + "</span> <span class='current-price'>$" +
+                productArray[i].currentPrice + "</span></p></a></div>";
         }
     }
 
